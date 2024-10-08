f01433b43a4f846ee7b2100f2974cd1330e3be70
Aicnal <69223323+Aicnal@users.noreply.github.com>
2024-10-08 20:03:39 +0800

    Update: domain exchange

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari-bec.pages.dev/",
+  site: "https://duckk.org/",
   base: "/",
   trailingSlash: "always",
   integrations: [
