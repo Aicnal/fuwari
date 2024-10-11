f0b36ce765ef2a0eb8c77d392cb991ca816a4489
Aicnal <69223323+Aicnal@users.noreply.github.com>
2024-10-12 01:36:18 +0800

    Update file

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Duck\'s Blog',
-  subtitle: 'A blog about tech, bio and life.',
+  subtitle: 'A blog about my boring life.',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
