69970d97e6ab2512a271bbeac5dbaa5c454a3160
Aicnal <69223323+Aicnal@users.noreply.github.com>
2024-10-15 00:56:03 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,6 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import {parseDirectiveNode} from "./src/plugins/remark-directive-rehype.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
 import {remarkExcerpt} from "./src/plugins/remark-excerpt.js";
-import remarkToc from 'remark-toc';
 
 const oklchToHex = (str) => {
   const DEFAULT_HUE = 250
